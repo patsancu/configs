20d23ee7db21b2edcc9d3665b62534068f6662fe
Patrick <patrick@sanzo.es>
2017-06-05 20:22:45 +0200

    needed to change path

--- tmux/sessions/work/tvmetrix_sql.sh
+++ tmux/sessions/work/tvmetrix_sql.sh
@@ -6,7 +6,7 @@ SESSION_NAME=tvmetrix-sql
 tmux has-session -t $SESSION_NAME 2>/dev/null
 if [ "$?" -eq 1 ] ; then
     echo "No Session found.  Creating and configuring."
-    source ~/Configs/tmux/sessions/tvmetrix_sql_create_session.sh $SESSION_NAME
+    source ~/Configs/tmux/sessions/work/tvmetrix_sql_create_session.sh $SESSION_NAME
 else
     echo "Session found.  Connecting."
     tmux attach-session -t $SESSION_NAME
