ac34b212ba399e2d2d33fd3fedbb67b10e4bc0bf
Patrick Sanzo <patricksanzo@gmail.com>
2023-03-29 17:32:02 +0200

    vim: Add to sh template set -euo pipefail

--- vim/config/templates/skeleton.sh
+++ vim/config/templates/skeleton.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+# -e option immediately exits if any command has a non-zero exit status
+# -u a reference to any variable you haven't previously defined
+# -o pipefail if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
+set -euo pipefail
