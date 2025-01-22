99c4a9adc91a75fb6d8954652b098f35154e12e2
Patrick Sanzo <patricksanzo@gmail.com>
2025-01-22 20:01:15 +0100

    macos: Add imagemagick

--- post-install-macos.sh
+++ post-install-macos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-packages=( zsh coreutils tmux just watch meld npm)
+packages=( zsh coreutils tmux just watch meld npm imagemagick)
 
 for package in "${packages[@]}"; do
     brew install $package
