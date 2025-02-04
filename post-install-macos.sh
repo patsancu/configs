068956a8bcdaf45206ed8ad90b9f76869f0d8faa
Patrick Sanzo <patricksanzo@gmail.com>
2025-02-04 17:21:15 +0100

    packages: Add macos ag

--- post-install-macos.sh
+++ post-install-macos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-packages=( zsh coreutils tmux just watch meld npm imagemagick)
+packages=( zsh coreutils tmux just watch meld npm imagemagick ag)
 
 for package in "${packages[@]}"; do
     brew install $package
