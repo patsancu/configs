ee4960f3c38c2a28a6b05e3f840a776d21079969
Patrick <patricksanzo@gmail.com>
2025-11-01 22:15:21 +0100

    macos: Add nvm to packages

--- post-install-macos.sh
+++ post-install-macos.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 packages=( zsh coreutils tmux just watch meld npm imagemagick ag)
+packages=(${packages[@]} nvm)
 
 for package in "${packages[@]}"; do
     brew install $package
