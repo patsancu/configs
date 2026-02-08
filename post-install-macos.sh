cbd3fd09022de3a37c7a7c5051353157ee5bebd6
Patrick <patricksanzo@gmail.com>
2026-02-08 18:49:46 +0100

    macos: Add wget gpg bash to packages

--- post-install-macos.sh
+++ post-install-macos.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-packages=( zsh coreutils tmux just watch meld npm imagemagick ag)
-packages=(${packages[@]} nvm)
+packages=( zsh coreutils tmux just watch meld npm imagemagick ag wget gpg)
+packages=(${packages[@]} xpaint)
+
+# macos' bash is veeery old
+packages=(${packages[@]} bash)
 
 for package in "${packages[@]}"; do
     brew install $package
