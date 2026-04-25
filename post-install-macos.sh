881a8876394b5045a88ae00dff1069cd41908fe8
Patrick <patricksanzo@gmail.com>
2026-04-25 20:18:03 +0200

    macos: Add gh brew package

--- post-install-macos.sh
+++ post-install-macos.sh
@@ -4,7 +4,7 @@ packages=( zsh coreutils tmux just watch meld npm imagemagick ag wget gpg)
 packages=(${packages[@]} xpaint)
 packages=(${packages[@]} ghostscript) # for imagemagick
 packages=(${packages[@]} glow) # for fancy rendering of markdown, like "less"
-
+packages=(${packages[@]} gh)
 
 # macos' bash is veeery old
 packages=(${packages[@]} bash)
