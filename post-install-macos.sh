38af137b14984cba08a21ca24023ead087fad475
Patrick <patricksanzo@gmail.com>
2026-05-21 17:54:21 +0200

    macos packages: add nmap

--- post-install-macos.sh
+++ post-install-macos.sh
@@ -4,7 +4,9 @@ packages=( zsh coreutils tmux just watch meld npm imagemagick ag wget gpg)
 packages=(${packages[@]} xpaint)
 packages=(${packages[@]} ghostscript) # for imagemagick
 packages=(${packages[@]} glow) # for fancy rendering of markdown, like "less"
-packages=(${packages[@]} gh)
+packages=(${packages[@]} gh) # github
+packages=(${packages[@]} nmap) # if I want to list devices attached to my network
+
 
 # macos' bash is veeery old
 packages=(${packages[@]} bash)
