d3a3f453f498854cdeced5865f9c13d2e9fbe6bb
Patrick <patrick@sanzo.es>
2026-05-29 17:06:05 +0000

    packages: Source scripts because they don't have x permission

--- post-install-packages.sh
+++ post-install-packages.sh
@@ -4,8 +4,8 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "Using linux, will install packages, debian style"
-    "${script_dir}/post-install-packages-ubuntu.sh"
+    source "${script_dir}/post-install-packages-ubuntu.sh"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Using macos, will install packages with brew"
-    "${script_dir}/post-install-packages-macos.sh"
+    source "${script_dir}/post-install-packages-macos.sh"
 fi
