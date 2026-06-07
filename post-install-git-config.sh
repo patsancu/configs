2fe79aded0372e4b097c348740285f9f4244f1ac
Patrick <patricksanzo@gmail.com>
2026-06-07 21:16:40 +0200

    install: Add missing config folder

--- post-install-git-config.sh
+++ post-install-git-config.sh
@@ -3,7 +3,9 @@
 echo "********************"
 echo "Git stuff"
 echo "-------------------"
+CONFIG_FOLDER="$(dirname "$(realpath "$0")")"
 GIT_CONFIG_DESTINATION=$HOME/.gitconfig
+echo "Will link $CONFIG_FOLDER/.gitconfig to $GIT_CONFIG_DESTINATION"
 if [ ! -f $GIT_CONFIG_DESTINATION ]; then
     ln -s $CONFIG_FOLDER/.gitconfig $GIT_CONFIG_DESTINATION
     echo "file created"
